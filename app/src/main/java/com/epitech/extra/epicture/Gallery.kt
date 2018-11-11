7800ed4af406c4717de89f426150349a4a8e824f
Anthony <anthony.tram@epitech.eu>
2018-11-11 22:02:09 +0100

    Auto stash before merge of "apiRead" and "origin/apiRead"

--- app/src/main/java/com/epitech/extra/epicture/Gallery.kt
+++ app/src/main/java/com/epitech/extra/epicture/Gallery.kt
@@ -1,10 +1,75 @@
 package com.epitech.extra.epicture
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import com.epitech.extra.epicture.R.layout.gallery
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.content_main.hello_world_id
 import kotlinx.android.synthetic.main.content_main.*
+import java.io.InputStream
 
 class Gallery : PageTemp("Gallery") {
+
+
+    internal class DownloadImageTask(var pic: ImageView) : AsyncTask<String, Void, Bitmap>() {
+        override fun doInBackground(vararg urls: String): Bitmap? {
+            val urldisplay = urls[0]
+            var mIcon11: Bitmap? = null
+            try {
+                val `in` = java.net.URL(urldisplay).openStream()
+                mIcon11 = BitmapFactory.decodeStream(`in`)
+            } catch (e: Exception) {
+                Log.e("Error", e.message)
+                e.printStackTrace()
+            }
+
+            return mIcon11
+        }
+
+        override fun onPostExecute(result: Bitmap) {
+            pic.setImageBitmap(result)
+        }
+    }
+
+    class ProgrammingAdapter(private val data: Array<String>) :
+        RecyclerView.Adapter<ProgrammingAdapter.ProgrammingViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProgrammingViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val view = inflater.inflate(R.layout.gallery, parent, false)
+            return ProgrammingViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ProgrammingViewHolder, position: Int) {
+            val title = data[position]
+            holder.txttitle.text = title
+        }
+
+        override fun getItemCount(): Int {
+            return data.size
+        }
+
+        inner class ProgrammingViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+            internal var imgIcon: ImageView
+            internal var txttitle: TextView
+
+            init {
+                imgIcon = itemView.findViewById(R.id.picture) as ImageView
+                Picasso.get().load("https://i.imgur.com/H981AN7.jpg").into(imgIcon)
+                txttitle = itemView.findViewById(R.id.txttitle) as TextView
+            }
+        }
+
+    }
 
 }
--- app/src/main/java/com/epitech/extra/epicture/MainActivity.kt
+++ app/src/main/java/com/epitech/extra/epicture/MainActivity.kt
@@ -13,8 +13,18 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.nav_header_main.*
 import android.os.StrictMode
+import android.support.constraint.ConstraintLayout
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.gallery.*
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.lang.Exception
 
 
 
@@ -92,11 +102,21 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             }
 
             R.id.nav_gallery -> {
-               // val gallery = Gallery()
-                // hello_world_id.text = gallery.retClassName()
-
+                /*
+               val newtext = findViewById<TextView>(R.id.hello_world_id) //gallery.retClassName()
+            newtext.setText("hello darkness my old friend")
+                setContentView(R.layout.gallery)
+                val pic = findViewById<ImageView>(R.id.picture)
+                Picasso.get().load("https://i.imgur.com/H981AN7.jpg").into(picture)*/
+                setContentView(R.layout.gallery_back)
+                val langagtes = arrayOf("Don't", "Blame", "Me")
+                val a = findViewById<RecyclerView>(R.id.recycler_view)
+                a.setLayoutManager(LinearLayoutManager(this))
+                val gallery = Gallery.ProgrammingAdapter(langagtes)
+                val imgur = Imgur()
+                val img_list = imgur.getImagesUser();
+                a.setAdapter(gallery)
             }
-
             R.id.nav_slideshow -> {
 
             }
